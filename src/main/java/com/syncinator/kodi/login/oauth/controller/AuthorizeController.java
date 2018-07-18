c1b9cc4b928e48041595f831232a68182837a83d
Carlos Guzman <carlosguzmang@hotmail.com>
2018-07-17 21:13:12 -0400

    QRcode url

--- src/main/java/com/syncinator/kodi/login/oauth/controller/AuthorizeController.java
+++ src/main/java/com/syncinator/kodi/login/oauth/controller/AuthorizeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,6 +23,11 @@ public class AuthorizeController {
 	
 	@Autowired
 	private ApplicationContext context;
+	
+	@RequestMapping("/signin/{pin}")
+	public String signin(@PathVariable String pin, Model model, HttpServletRequest request) throws IOException {
+		return login(pin, model, request);
+	}
 	
 	@RequestMapping("/authorize")
 	public String login(@RequestParam String pin, Model model, HttpServletRequest request) throws IOException {
