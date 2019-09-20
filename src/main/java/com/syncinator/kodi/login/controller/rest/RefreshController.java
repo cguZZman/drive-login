a10e651217079eb40d4bfbf21f6ab3094d0664ee
Carlos Guzman <cguzmangarcia@deloitte.com>
2019-09-20 19:02:32 -0400

    fix

--- src/main/java/com/syncinator/kodi/login/controller/rest/RefreshController.java
+++ src/main/java/com/syncinator/kodi/login/controller/rest/RefreshController.java
@@ -3,6 +3,7 @@ package com.syncinator.kodi.login.controller.rest;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,20 +26,29 @@ import com.syncinator.kodi.login.oauth.provider.Provider;
 @RequestMapping("/refresh")
 public class RefreshController {
 
-	@SuppressWarnings("unused")
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private ApplicationContext context;
 
 	@RequestMapping(method = RequestMethod.POST)
-	public Map<String,Object> refresh(@RequestParam String provider, @RequestParam(name="refresh_token") String refreshToken) throws Exception {
+	public Map<String,Object> refresh(@RequestParam(required = false) String provider, 
+			@RequestParam(name="refresh_token", required = false) 
+		String refreshToken, HttpServletResponse response) throws Exception {
+		logger.info("Provider: " + provider);
+		if (StringUtils.isEmpty(provider) || StringUtils.isEmpty(refreshToken)) {
+			logger.error(refreshToken);
+			response.sendError(HttpStatus.BAD_REQUEST.value(), "Provider and refresh token required");
+			return null;
+		}
 		Provider connector = context.getBean(Provider.NAME_PREFIX + provider, Provider.class);
 		return connector.tokens(Provider.GRANT_TYPE_REFRESH_TOKEN, refreshToken);
 	}
 
 
 	@ExceptionHandler(NoSuchBeanDefinitionException.class)
-	public void exceptionHandler(HttpServletResponse response, NoSuchBeanDefinitionException e) throws IOException{
+	public void exceptionHandler(HttpServletRequest request, HttpServletResponse response, NoSuchBeanDefinitionException e) throws IOException{
+		logger.error("NoSuchBeanDefinitionException: " + e.getBeanName());
+		logger.error(request.getParameterMap().toString());
 		if (e.getBeanName().startsWith(Provider.NAME_PREFIX)) {
 			response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid provider '"+e.getBeanName()+"'");
 			return;
@@ -47,6 +58,8 @@ public class RefreshController {
 	
 	@ExceptionHandler(HttpClientErrorException.class)
 	public void exceptionHandler(HttpServletResponse response, HttpClientErrorException e) throws IOException{
+		logger.error("HttpClientErrorException: " + e.getStatusCode().value());
+		logger.error(e.getStatusText());
 		response.sendError(e.getStatusCode().value(), e.getStatusText());
 		return;
 	}
